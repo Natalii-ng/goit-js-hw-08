03c393e5dc091872d0fdc1a3340f797b283a38cb
natalia <geracimova.ng@gmail.ru>
2023-01-05 14:19:49 +0200

    s

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 player.setCurrentTime(localStorage.getItem('setCurrentTime(|)') || 0);
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 500));
 
 function onPlay({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds);
