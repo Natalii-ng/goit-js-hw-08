baf5551445c72341a9f448803bb2d446aafe5534
natalia <geracimova.ng@gmail.ru>
2023-01-05 14:11:06 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+player.setCurrentTime(localStorage.getItem('setCurrentTime(|)') || 0);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
 
