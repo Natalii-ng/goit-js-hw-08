63bd2fb42a066f76602c20e7b6335f4c64d8a65f
natalia <geracimova.ng@gmail.ru>
2023-01-06 14:48:09 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,20 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
 function onPlay({ seconds }) {
 localStorage.setItem('videoplayer-current-time', seconds);
-}
+};
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time', 0));
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.then(function (seconds) {})
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+
+            default:
+                break;
+        }
+    });
