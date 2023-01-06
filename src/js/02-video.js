d0e1a9ebd84950e84f9849918a0ddbd8232413f1
natalia <geracimova.ng@gmail.ru>
2023-01-06 12:10:47 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
-player.setCurrentTime(localStorage.getItem('setCurrentTime(|)') || 0);
-player.on('timeupdate', throttle(onPlay, 500));
+
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+localStorage.setItem('videoplayer-current-time', seconds);
 }
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time', 0));
